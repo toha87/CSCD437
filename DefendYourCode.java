bcfca15d38191f9b6a980c1a846a3fda450bb316
Gregory Ostanin <ostingreg@gmail.com>
2019-07-21 16:49:37 -0700

    Added fullname, two values functionality I added two secure methods that check input. I enclosed each user prompt in a while loop for modularity.

--- DefendYourCode.java
+++ DefendYourCode.java
@@ -1,15 +1,56 @@
+import java.sql.SQLOutput;
 import java.util.*;
 import java.security.SecureRandom;
+import java.util.regex.Pattern;
+
 public class DefendYourCode {
 
     public static void main(String[] args) {
-
         Scanner kb = new Scanner(System.in);
+        boolean done = false;
+        String spare = null;
+        String fname = null;
+        String lname = null;
+        long firstValue = 0;
+        long secondValue = 0;
 
-        System.out.print("Please enter password: ");
-        String password = kb.nextLine();
+        while (!done) {
+            System.out.println("Enter first name(minimum 2 characters, only letters):");
+            fname = kb.nextLine().trim();
+            done = name(fname);
+        }
+        done = false;
+        while (!done) {
+            System.out.println("Enter last name(minimum 2 characters, only letters):");
+            lname = kb.nextLine().trim();
+            done = name(lname);
+        }
+        done = false;
+        while (!done) {
+            System.out.println("Enter first number[-2147483648, 2147483647]:");
+            spare = kb.nextLine();
+            if(values(spare)) {
+                firstValue = Long.valueOf(spare);
+                done = true;
+            }
+        }
+        done = false;
+        while (!done) {
+            System.out.println("Enter second number[-2147483648, 2147483647]:");
+            spare = kb.nextLine();
+            if(values(spare)) {
+                secondValue = Long.valueOf(spare);
+                done = true;
+            }
+        }
 
-        Password(password);
+        done = false;
+        while(!done) {
+            System.out.print("Please enter password: ");
+            String password = kb.nextLine();
+
+            Password(password);
+        }
     }
 
     private static void Password(String password) {
@@ -22,5 +63,17 @@ public class DefendYourCode {
 
     }
 
+    private static boolean name(String name) {
+        return Pattern.matches("\\s*^[a-zA-Z]{2,50}$\\s*", name);
+    }
 
+    private static boolean values(String value) {
+        try {
+            int number = Integer.valueOf(value);
+            return true;
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
