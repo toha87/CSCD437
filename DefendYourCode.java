2ec37b518f2cf01f5e1c6d0126b8c4a380178dcd
Anatoli Railean <arailean@eagles.ewu.edu>
2019-07-20 18:45:46 -0700

    Added Salt

--- DefendYourCode.java
+++ DefendYourCode.java
@@ -1,6 +1,26 @@
+import java.util.*;
+import java.security.SecureRandom;
 public class DefendYourCode {
 
-public static void main(String[] args) {
-   
-   }
+    public static void main(String[] args) {
+
+        Scanner kb = new Scanner(System.in);
+
+        System.out.print("Please enter password: ");
+        String password = kb.nextLine();
+
+        Password(password);
+    }
+
+    private static void Password(String password) {
+
+        SecureRandom rand = new SecureRandom();
+
+        String salt = Integer.toString(rand.nextInt(1000));
+
+        System.out.println(password + salt);
+
+    }
+
+
 }
