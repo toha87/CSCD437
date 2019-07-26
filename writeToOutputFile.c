12230f82d4b739641e17a2c5ce54854736b28f76
Anatoli Railean <arailean@eagles.ewu.edu>
2019-07-25 20:31:28 -0700

    Changed Read from File and Write to a File

--- writeToOutputFile.c
+++ writeToOutputFile.c
@@ -3,34 +3,21 @@
 
 void writeToOutputFile(char fname[], char lname[], float add, float mult, char inputFname[], char outputFname[])
 {
-
-// https://stackoverflow.com/questions/3747086/reading-the-whole-text-file-into-a-char-array-in-c
-
    FILE *fptrInput;
    FILE *fptrOutput;
-   long lSize;
+   size_t size = 0;
    char *buffer;
 
    fptrInput = fopen(inputFname, "r");
-   if( !fptrInput ) perror(fptrInput),exit(1);
-
-   fseek( fptrInput , 0L , SEEK_END);
-   lSize = ftell( fptrInput );
-   rewind( fptrInput );
-
-   buffer = calloc( 1, lSize+1 );
-   if( !buffer ) fclose(fptrInput),fputs("memory alloc fails",stderr),exit(1);
-
-   if( 1!=fread( buffer , lSize, 1 , fptrInput) )
-   fclose(fptrInput),free(buffer),fputs("entire read fails",stderr),exit(1);
+   fseek(fptrInput, 0, SEEK_END);
+   size = ftell(fptrInput);
+   rewind(fptrInput);
+   buffer = malloc((size + 1) * sizeof(*buffer));
+   fread(buffer, size, 1, fptrInput);
+   buffer[size] = '\0';
 
    fptrOutput = fopen(outputFname, "w");
-
-   fputc (fname, fptrOutput);
-   fputc (lname, fptrOutput);
-   fputc (add, fptrOutput);
-   fputc (mult, fptrOutput);
-   fputc (buffer, fptrOutput);
+   fprintf(fptrOutput, "%s %s\n %f\n %f\n %s", fname, lname, add, mult, buffer);
 
    fclose(fptrInput);
    fclose(fptrOutput);
